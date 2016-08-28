81ff43256f8b2044ab14bb08e229a77b412fe531
Dave Cooper <david@developersheffield.co.uk>
2016-08-28 13:05:01 +0100

    Version 1 of image optimisation batch file

---
+++ go.bat
@@ -0,0 +1,27 @@
+:: Recursively create a crushed version of each png file
+FOR /r %~dp0 %%c in (*.png) DO (
+	SET n="%%~pc%%~nc-min.png"
+	%~dp0\utils\pngcrush.exe -brute "%%c" "%%n"
+	
+	IF %%~zn GTR %%~zc (
+		:: new file has larger filesize than existing file so just delete it
+		DEL "%%n"
+	) ELSE (
+		:: new file has smaller filesize than existing file delete existing file and rename new file to original name
+		MOVE /y "%%n" "%%c"
+	)
+)
+
+:: Recursively create a crushed version of each jpg file
+FOR /r %~dp0 %%c in (*.jpg) DO (
+	SET n="%%~pc%%~nc-min.jpg"
+	%~dp0\utils\jpegtran.exe -optimize "%%c" "%%n"
+	
+	IF %%~zn GTR %%~zc (
+		:: new file has larger filesize than existing file so just delete it
+		DEL "%%n"
+	) ELSE (
+		:: new file has smaller filesize than existing file delete existing file and rename new file to original name
+		MOVE /y "%%n" "%%c"
+	)
+)
